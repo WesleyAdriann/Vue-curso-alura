ff8748d63f93b80c169ba13d22be1a03fba8d78c
Wesley Adriann <wesleyadriann@gmail.com>
2019-05-10 13:12:14 -0300

    Aula 02 - 04

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img :src="foto.src" :alt="foto.alt"/>
+    <ul>
+      <li v-for="foto of fotos" :key="foto.alt">
+        <img :src="foto.url" :alt="foto.alt"/>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,10 +16,16 @@ export default {
   data () {
     return {
       titulo: "Alura app",
-      foto : {
-        src : "./assets/logo.png",
-        alt : "Logo",
-      }
+      fotos : [
+        {
+          url : "https://cdn-images-1.medium.com/max/1600/1*wqYF-8Dmh7LhtLkKfERc3Q.png",
+          titulo : "Logo"
+        },
+        {
+          url : "https://cdn-images-1.medium.com/max/1600/1*wqYF-8Dmh7LhtLkKfERc3Q.png",
+          titulo : "logo2"
+        }
+      ]
     }
   }
 }
