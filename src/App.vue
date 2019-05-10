ba6503f59af584ea394d8c751ce4f595de0e8b8d
Wesley Adriann <wesleyadriann@gmail.com>
2019-05-10 13:00:00 -0300

    Aula 02 - 01

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <img :src="foto.src" :alt="foto.alt"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      titulo: "Alura app",
+      foto : {
+        src : "./assets/logo.png",
+        alt : "Logo",
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
