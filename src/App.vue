ce6c0281aaa49935854c04050549cbe7090ef04b
Wesley Adriann <wesleyadriann@gmail.com>
2019-05-10 13:24:50 -0300

    Aula 03 - 05

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,11 @@ export default {
   data () {
     return {
       titulo: "Alura app",
-      fotos : [
-        {
-          url : "https://cdn-images-1.medium.com/max/1600/1*wqYF-8Dmh7LhtLkKfERc3Q.png",
-          titulo : "Logo"
-        },
-        {
-          url : "https://cdn-images-1.medium.com/max/1600/1*wqYF-8Dmh7LhtLkKfERc3Q.png",
-          titulo : "logo2"
-        }
-      ]
+      fotos : []
     }
+  },
+  created() {
+    alert("Componente Criado");
   }
 }
 </script>
