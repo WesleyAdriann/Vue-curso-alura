dc16350bb176782a4cd76d784aa973f32153d3a6
Wesley Adriann <wesleyadriann@gmail.com>
2019-05-10 15:27:19 -0300

    Aula 06 - 03

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,11 @@
 <template>
-  <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="Busca"/>
-    
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of filtroFotos" :key="foto.alt">
-        <Painel :titulo="foto.titulo">
-          <ImagemResponsiva :url="foto.url" :titulo="foto.titulo"/>
-        </Painel>
 
-      </li>
-    </ul>
-  </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
-  data () {
-    return {
-      titulo: "Alura app",
-      fotos : [],
-      filtro : "",
-    }
-  },
-  created() {
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos);
-  },
-  components: {
-    Painel,
-    ImagemResponsiva,
-  },
-  computed : {
-    filtroFotos () {
-      if(this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      }else {
-        return this.fotos;
-      }
-    }
-  }
+  
 }
 </script>
 
@@ -53,18 +14,5 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
-  }
-  .centralizado {
-    text-align: center;
-  }
-  .lista-fotos {
-    list-style: none; 
-  }
-  .lista-fotos .lista-fotos-item{
-    display: inline-block;
-  }
-  .filtro {
-    display: block;
-    width: 100%;
   }
 </style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="Busca"/>
+    
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of filtroFotos" :key="foto.alt">
+        <Painel :titulo="foto.titulo">
+          <ImagemResponsiva :url="foto.url" :titulo="foto.titulo"/>
+        </Painel>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from '../shared/painel/Painel.vue';
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+
+export default {
+  data () {
+    return {
+      titulo: "Alura app",
+      fotos : [],
+      filtro : "",
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos);
+  },
+  components: {
+    Painel,
+    ImagemResponsiva,
+  },
+  computed : {
+    filtroFotos () {
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      }else {
+        return this.fotos;
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  
+  .centralizado {
+    text-align: center;
+  }
+  .lista-fotos {
+    list-style: none; 
+  }
+  .lista-fotos .lista-fotos-item{
+    display: inline-block;
+  }
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+</style>
