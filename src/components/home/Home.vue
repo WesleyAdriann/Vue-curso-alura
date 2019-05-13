c1e0d0337e353841d9e9d1f8c77ac6ae9571f83e
Wesley Adriann <wesleyadriann@gmail.com>
2019-05-13 13:46:19 -0300

    pt 2 Aula 03 - 02

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -57,8 +57,11 @@ export default {
   methods : {
     remove(foto) {
       this.$http.delete(`http://localhost:3000/v1/fotos/${foto._id}`)
-      .then(() => this.mensagem = "Foto removida com sucesso",
-        err => {
+      .then(() => {
+        let indice = this.fotos.indexOf(foto);
+        this.fotos.splice(indice, 1);
+        this.mensagem = "Foto removida com sucesso";
+      }, err => {
           console.log(err);
           this.mensagem = "Não foi possivel remover";
         });
