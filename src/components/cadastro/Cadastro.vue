dbd58bf773f26d4378ebf2595c924f81f12ebca6
Wesley Adriann <wesleyadriann@gmail.com>
2019-05-13 13:18:08 -0300

    pt2 Aula 02 - 06

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -31,6 +31,7 @@
 <script>
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
 import Botao from '../shared/botao/Botao.vue';
+import Foto from '../../domain/foto/Foto'
 
 export default {
   components: {
@@ -40,16 +41,13 @@ export default {
   },
   data () {
       return {
-          foto : {
-              titulo : '',
-              url: '',
-              descricao: '',
-          }
+          foto : new Foto()
       }
   },
   methods : {
       grava() {
           console.log(this.foto);
+          this.foto = new Foto;
       }
   }
 }
