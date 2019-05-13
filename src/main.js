c1f825cce9807166fdeb5bef37d6ab25a3d51048
Wesley Adriann <wesleyadriann@gmail.com>
2019-05-13 15:52:54 -0300

    pt2 Aula 05

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
-import VeeValidate from 'vee-validate';
 import { routes } from "./routes";
 import './directives/Transform';
+
+import VeeValidate, { Validator } from 'vee-validate';
+import pt_BR from 'vee-validate/dist/locale/pt_BR';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000/'
@@ -14,6 +16,10 @@ const router = new VueRouter({
   routes,
   mode : 'history'
 });
+
+Vue.use(VeeValidate);
+Validator.localize('pt_BR', pt_BR);
+
 
 Vue.config.productionTip = false
 
